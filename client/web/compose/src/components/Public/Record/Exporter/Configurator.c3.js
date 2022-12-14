15f9bba14774db8a77f160e2fe7d4e75b5019d16
Katrin Yordanova <katrin.yordanova@crust.tech>
2022-12-15 00:08:33 +0200

    Start fixing Exporter/Configurator

--- client/web/compose/src/components/Public/Record/Exporter/Configurator.c3.js
+++ client/web/compose/src/components/Public/Record/Exporter/Configurator.c3.js
@@ -1,16 +1,18 @@
 // eslint-disable-next-line
 import { default as component } from './Configurator.vue'
 import { compose } from '@cortezaproject/corteza-js'
+import { components } from '@cortezaproject/corteza-vue'
+const { checkbox, select } = components.C3.controls
 
-// TypeError: this.module.systemFields is not a function"
+// errors to fix
 const props = {
   allowJSON: true,
   allowCSV: true,
   module: new compose.Module(),
   preselectedFields: [],
-  recordCount: 0,
+  recordCount: 5,
   query: undefined,
-  selection: [],
+  // selection: [],
   filterRangeType: 'all',
   filterRangeBy: 'createdAt',
   dateRange: 'lastMonth',
@@ -18,6 +20,9 @@ const props = {
   endDate: null,
   systemFields: ['ownedBy', 'createdAt', 'createdBy', 'updatedAt', 'updatedBy'],
   disabledTypes: ['User', 'Record', 'File'],
+  processingCount: false,
+  rangeRecordCount: 5,
+  selectedFields: [],
 }
 
 export default {
@@ -25,5 +30,32 @@ export default {
   group: ['Exporter'],
   component,
   props,
-  controls: [],
+  controls: [
+    checkbox('Allow JSON', 'allowJSON'),
+    checkbox('Allow CSV', 'allowCSV'),
+    select('Module', 'module', [{ value: new compose.Module(), text: 'new compose.Module()' }, undefined]),
+    select('Filter range by', 'filterRangeBy', [{ value: 'created_at', text: 'created_at' }, { value: 'deleted_at', text: 'deleted_at' }]),
+  ],
+  scenarios: [
+    {
+      label: 'Full form',
+      props,
+    },
+    {
+      label: 'Empty form',
+      props: {
+        ...props,
+        allowJSON: false,
+        allowCSV: false,
+        module: undefined,
+        recordCount: 0,
+        selection: [],
+        filterRangeBy: '',
+        filterRangeType: '',
+        dateRange: '',
+        systemFields: [],
+        disabledTypes: [],
+      },
+    },
+  ],
 }
