a87547205ded1a18556ae97d4c38bc646c927ed6
Emmy Leke <emmy.leke@planetcrust.com>
2023-08-02 11:43:54 +0100

    Eliminate duplicate keys error for Tabs

--- client/web/compose/src/components/PageBlocks/TabsConfigurator.vue
+++ client/web/compose/src/components/PageBlocks/TabsConfigurator.vue
@@ -361,8 +361,8 @@ export default {
       return title || kind
     },
 
-    getOptionKey ({ blockID }) {
-      return blockID
+    getOptionKey (block) {
+      return fetchID(block)
     },
 
     setDefaultValues () {
